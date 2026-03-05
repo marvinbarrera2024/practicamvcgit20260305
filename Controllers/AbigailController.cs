f4efe85d726df27cfdc280ed853faa6e5694ca82
abigail23181 <abigailrodrigxz@gmail.com>
2026-03-05 17:52:28 +0000

    Abigail cree el controlador

---
+++ Controllers/AbigailController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using practicamvcgit20260305.Models;
+
+namespace practicamvcgit20260305.Controllers;
+
+public class AbigailController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }gi
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}

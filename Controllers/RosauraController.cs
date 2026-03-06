638c216af048f673652bdc1ca6528dabd9244756
Rosaura <Karlarosauras@gmail.com>
2026-03-05 22:15:59 -0600

    Rosaura nuevo cambio

---
+++ Controllers/RosauraController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace practicamvcgit20260305.Controllers
+{
+    public class RosauraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

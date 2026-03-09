776387b90096832788a0d968e84a491ad0bc86a7
Kevin Ernesto Meza Garcia <erasmomeza11@gmail.com>
2026-03-08 21:47:56 -0600

    Practica calculadora

---
+++ Controllers/kevinController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Reciclajejuego.AppMVC.Controllers
+{
+    public class kevinController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(double num1, double num2, string operacion)
+        {
+            double resultado = 0;
+
+            switch (operacion)
+            {
+                case "sumar":
+                    resultado = num1 + num2;
+                    break;
+
+                case "restar":
+                    resultado = num1 - num2;
+                    break;
+
+                case "multiplicar":
+                    resultado = num1 * num2;
+                    break;
+
+                case "dividir":
+                    if (num2 != 0)
+                        resultado = num1 / num2;
+                    else
+                        ViewBag.Error = "No se puede dividir entre 0";
+                    break;
+            }
+
+            ViewBag.Resultado = resultado;
+
+            return View();
+        }
+    }
+}

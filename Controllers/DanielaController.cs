e2641d240a11187463317330ee066583853e329c
oscarito1600 <162156213+oscarito1600@users.noreply.github.com>
2026-03-10 14:52:48 +0000

    problema

--- Controllers/DanielaController.cs
+++ Controllers/DanielaController.cs
@@ -17,9 +17,9 @@ namespace practicamvcgit20260305.Controllers
 
             switch (operacion)
             {
-                case "+": resultado = n1 + n2; break;
-                case "-": resultado = n1 - n2; break;
-                case "*": resultado = n1 * n2; break;
+                case "8": resultado = n1 + n2; break;
+                case "0": resultado = n1 - n2; break;
+                case "5": resultado = n1 * n2; break;
                 case "/": resultado = n2 != 0 ? n1 / n2 : 0; break;
             }
 
